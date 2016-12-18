18d3d6a22893df3319e7943c3468060197bc0bdf
Mitesh Shah <mitesh@miteshshah.com>
2016-12-18 16:03:08 +0530

    Minimally working version :)

--- src/astar.js
+++ src/astar.js
@@ -1,7 +1,7 @@
 //jshint esnext:true
-window.onload = astar;
+window.onload = main;
 
-function astar() {
+function main() {
     var canvas = document.getElementById('astar');
     var ctx = canvas.getContext('2d');
 
@@ -11,7 +11,7 @@ function astar() {
     }
 
     const WIDTH = 20;
-    const N = 16;
+    const N = 32;
 
     /* Lets create a NxN grid */
     var grid = (function(n) {
@@ -26,6 +26,8 @@ function astar() {
     function Node(x, y) {
         var visited = false;
         var goal = false;
+        var start = false;
+        var fringe = false;
         var grid_x = x;
         var grid_y = y;
         var color = 'white';
@@ -33,11 +35,15 @@ function astar() {
         var weight = Math.floor(Math.random() * 8) + 1;
 
         function update() {
-            if (visited === true) {
-                color = 'blue';
-            }
             if (goal === true) {
                 color = 'red';
+            } else if (start === true) {
+                color = 'green';
+            } else if (visited === true) {
+                color = '#532abc';
+            } else if (fringe === true) {
+                console.log("reached here");
+                color = 'black';
             }
 
             ctx.fillStyle = color;
@@ -56,9 +62,47 @@ function astar() {
             goal = true;
         }
 
+        function setStartNode() {
+            start = true;
+        }
+
+        function setFringe() {
+            fringe = true;
+        }
+
+        function getSuccessorList() {
+            let x = [-1, 0, 1];
+            let y = [-1, 0, 1];
+            
+            let successors = [];
+            for (let i = x[0]; i < x.length; i++) {
+                for (let j = y[0]; j < y.length; j++) {
+                    let nb_x = grid_x + Number(i);
+                    let nb_y = grid_y + Number(j);
+                    if (grid[nb_x] == undefined || grid[nb_x][nb_y] == undefined) {
+                        continue;
+                    }
+                    let neighbor = grid[nb_x][nb_y];
+                    if (neighbor.visited === true) {
+                        continue;
+                    }
+                    successors.push(neighbor);
+                }
+            }
+            return successors;
+        }
+
         var publicAPI = {
+            x: grid_x,
+            y: grid_y,
             update: update,
             setVisited: setVisited,
+            setGoalNode: setGoalNode,
+            setStartNode: setStartNode,
+            setFringe: setFringe,
+            getSuccessorList: getSuccessorList,
+            goal: goal,
+            visited: visited,
         };
 
         return publicAPI;
@@ -73,12 +117,63 @@ function astar() {
     }
 
 
-    /* Main Loop */
-    grid[10][10].setVisited();
-    grid[10][10].update();
+    /* Set start and goal nodes */
+    var start = grid[26][19];
+    start.setStartNode();
+    start.update();
 
-    console.log(grid);
+    var goal = grid[4][3];
+    goal.setGoalNode();
+    goal.update();
 
+    /* A* search implementation */
+    function astarSearch(start) {
+        var closed = [];
+        var open = PriorityQueue();
+        var epoch = 0;
+        open.add(start);
+
+        while (open.nodes.length != 0){
+            epoch += 1;
+
+            let node = open.nodes.shift();
+            if (node.goal === true) {
+                console.log("Found the node!");
+                return node;
+            }
+
+            if (closed.indexOf(node) <= -1) {
+                closed.push(node);
+                node.setVisited();
+                setTimeout(node.update, (epoch * 50) % 1000);
+                let successors = node.getSuccessorList();
+                for (let i = 0; i < successors.length; i++) {
+                    let fringeNode = successors[i];
+                    fringeNode.setFringe();
+                    open.add(fringeNode);
+                    setTimeout(fringeNode.update, (epoch * 50 + (i+1) * 30) % 1000);
+                }
+            }
+
+        }
+        return false;
+    }
+
+    console.log(astarSearch(start));
+};
+
+var PriorityQueue = function() {
+    var that = {};
+    that.nodes = [];
+
+    that.add = function add (node) {
+        that.nodes.push(node);
+        that.nodes.sort(function (f, s) {
+            return s.weight - f.weight;
+        });
+    };
+
+    return that;
 };
 
 console.log('end');
